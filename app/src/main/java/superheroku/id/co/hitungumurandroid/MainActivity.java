1f89925cf474cf251ffd4203734b66ad2695764d
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 13:53:20 +0700

    add

--- app/src/main/java/superheroku/id/co/hitungumurandroid/MainActivity.java
+++ app/src/main/java/superheroku/id/co/hitungumurandroid/MainActivity.java
@@ -2,12 +2,63 @@ package superheroku.id.co.hitungumurandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    long a =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText et1 = (EditText) findViewById (R.id.editText1);
+        final EditText et2 = (EditText) findViewById (R.id.editText2);
+        final EditText et3 = (EditText) findViewById (R.id.editText3);
+        Button btn1  = (Button)   findViewById (R.id.button1)  ;
+        final TextView tv1 = (TextView) findViewById (R.id.textView1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = Integer.parseInt(et1.getText().toString());
+                int month = Integer.parseInt(et2.getText().toString());
+                int year = Integer.parseInt(et3.getText().toString());
+
+                tv1.setText(String.valueOf(MainActivity.this.getAge(year, month, day)));
+
+            }
+        });
+
+
     }
+
+
+    public int getAge (int _year, int _month, int _day) {
+
+        GregorianCalendar cal = new GregorianCalendar();
+        int y, m, d, a;
+
+        y = cal.get(Calendar.YEAR);
+        m = cal.get(Calendar.MONTH);
+        d = cal.get(Calendar.DAY_OF_MONTH);
+        cal.set(_year, _month, _day);
+        a = y - cal.get(Calendar.YEAR);
+        if ((m < cal.get(Calendar.MONTH))
+                || ((m == cal.get(Calendar.MONTH)) && (d < cal
+                .get(Calendar.DAY_OF_MONTH)))) {
+            --a;
+        }
+        if(a < 0)
+            throw new IllegalArgumentException("Age < 0");
+        return a;
+    }
+
 }
